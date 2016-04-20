39a28c3b791f2e22dc6154c32aefecdcc9fdf49b
Dean Friedland <dfmmalaw@gmail.com>
2016-04-20 21:17:07 +0000

    added procfile back and server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT;
+var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__angular-workshop-app + '/app'));
+app.use(express.static(__dirname + '/app'));
 
 // set the home page route
 app.get('/', function(req, res) {
